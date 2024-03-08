2db978be5e2b333bc1f8c22beed82fcf7a4d75fe
cade <river1988@mail.ru>
2024-03-09 01:00:49 +0400

    KS-1-1 объявление переменных

---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,9 @@
+package lesson_1
+
+fun main() {
+    val numberOfOrders = 75
+    val thankYouMessage = "Спасибо за ваш заказ! Надеемся увидеть вас снова!"
+
+    println(numberOfOrders)
+    println(thankYouMessage)
+}
