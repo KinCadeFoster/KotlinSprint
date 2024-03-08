5dd465ad60ad233b590c14b3d198972c47f6d693
cade <river1988@mail.ru>
2024-03-09 01:31:55 +0400

    KS-1-3 код отображения даты полета и дату посадки Гагарина

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+fun main(){
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    print("Гагарин выполнил полет в $year году, ")
+    print("в $hour часов ")
+    println("$minute минут.")
+
+    hour = 10
+    minute = 55
+
+    println("Время посадки космического аппарата в $hour:$minute.")
+
+}
