9a092a70206dad57dc5099bec49124bb781bb5be
cade <river1988@mail.ru>
2024-03-09 19:29:09 +0400

    KS-1-5 - вынес константы - поправил офрмление текста кода

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,12 +1,15 @@
 package lesson_1
 const val SECONDS_IN_SPACE: Short = 6480
+const val SECONDS_IN_MINUTE: Short = 60
+const val SECONDS_IN_HOUR: Short = 3600
+
 
 fun main() {
-    val fullSecondsInSpace = (SECONDS_IN_SPACE%60)
-    val fullMinutesInSpace = (SECONDS_IN_SPACE%3600)/60
-    val fullHoursInSpace = SECONDS_IN_SPACE/3600
+    val fullSecondsInSpace = (SECONDS_IN_SPACE % SECONDS_IN_MINUTE)
+    val fullMinutesInSpace = (SECONDS_IN_SPACE % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val fullHoursInSpace = SECONDS_IN_SPACE / SECONDS_IN_HOUR
 
-    println("Время проведенное в космосе: " + "%02d".format(fullHoursInSpace) +":"+ "%02d".format(fullMinutesInSpace)
-            +":"+ "%02d".format(fullSecondsInSpace))
+    println("Время проведенное в космосе: " + "%02d".format(fullHoursInSpace) + ":" + "%02d".format(fullMinutesInSpace)
+            + ":" + "%02d".format(fullSecondsInSpace))
 
 }
