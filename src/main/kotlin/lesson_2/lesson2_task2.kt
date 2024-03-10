caf9d0171eddf5820344223d56527b76750a5e06
cade <river1988@mail.ru>
2024-03-11 02:24:27 +0400

    KS-2-5 - вынес константы в переменные

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,14 +1,14 @@
 package lesson_2
 
-const val EMPLOYEE_COUNT = 50
-const val EMPLOYEE_SALARY = 30000
-const val INTERN_COUNT = 30
-const val INTERN_SALARY = 20000
-
 fun main(){
-    val permanentEmployeeSalary = EMPLOYEE_COUNT * EMPLOYEE_SALARY
-    val allSalary = permanentEmployeeSalary + (INTERN_COUNT * INTERN_SALARY)
-    val averageSalary = allSalary / (EMPLOYEE_COUNT + INTERN_COUNT)
+    val employeeCount = 50
+    val employeeSalary = 30000
+    val internCount = 30
+    val internSalary = 20000
+
+    val permanentEmployeeSalary = employeeCount * employeeSalary
+    val allSalary = permanentEmployeeSalary + (internCount * internSalary)
+    val averageSalary = allSalary / (employeeCount + internCount)
 
     println("Расходы на выплату зарплаты для постоянных сотрудников: ${String.format("%,d", permanentEmployeeSalary)}")
     println("Общие расходы по ЗП после прихода стажеров: ${String.format("%,d", allSalary)}")
