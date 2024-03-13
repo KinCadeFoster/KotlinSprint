b08d3f700c2334445bacbea29672bcee8eda778a
cade <river1988@mail.ru>
2024-03-13 22:52:12 +0400

    KS-3-4 Вывод ходов

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,15 @@
+package lesson_3
+
+fun main() {
+    var startMove = "E2"
+    var endMove = "E4"
+    var moveNumber = 1
+
+    println("$startMove-$endMove;$moveNumber")
+
+    startMove = "D2"
+    endMove = "D3"
+    moveNumber++
+
+    println("$startMove-$endMove;$moveNumber")
+}
