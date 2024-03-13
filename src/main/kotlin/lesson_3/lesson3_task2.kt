c6f984590cc1b3e7097ad62a629f09f85a356d92
cade <river1988@mail.ru>
2024-03-13 22:02:10 +0400

    KS-3-1 - Вывод ФИО и возраста

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,16 @@
+package lesson_3
+
+fun main() {
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    val oldLastName = "Андреева"
+    val lastName = "Содоров"
+    val age = 20
+
+    println("$oldLastName $name $patronymic, $age лет")
+    println("$lastName $name $patronymic, ${age + 2} года")
+}
+
+
+
+
