bb13f857996c80088232e4367a4b51be69c0f98f
cade <river1988@mail.ru>
2024-03-14 22:05:17 +0400

    KS-3-1 переиспользование одной переменной для преветствия

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -2,12 +2,10 @@ package lesson_3
 
 fun main() {
     val userName = "Василий"
-    val dayGreetings = ", добрый день! "
-    val nightGreetings = ", доброй ночи! "
 
-    var greetings: String = userName + dayGreetings
-    println(greetings)
+    var greetings = "добрый день"
+    println("$userName, $greetings!")
 
-    greetings = userName + nightGreetings
-    println(greetings)
+    greetings = "доброй ночи"
+    println("$userName, $greetings!")
 }
