aec37d641842137e7fa73d970784c83e7f2a6a88
cade <river1988@mail.ru>
2024-03-16 10:06:52 +0400

    KS-5-1 -реализация капчи

---
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -0,0 +1,9 @@
+package lesson_5
+
+fun main(){
+    print("Решите пример: 5 + 1\nВведите ответ: ")
+    val answer = readln().toInt()
+
+    if (answer == 6) println("Добро пожаловать!")
+    else println("Доступ запрещен.")
+}
