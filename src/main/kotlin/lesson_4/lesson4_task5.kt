dd6bcc3b207eec2633c47dad22933ea227c66c96
cade <river1988@mail.ru>
2024-03-28 09:26:33 +0400

    KS-4-5 - вынес константы - переформатировал условия

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,11 +1,22 @@
 package lesson_4
 
-fun main(){
-    val hasHullDamage: Boolean = readln().toBoolean()
+const val SHIP_NOT_DAMAGED = true
+const val MIN_CREW_MEMBERS = 55
+const val MAX_CREW_MEMBERS = 70
+const val MIN_PROVISION_COUNT = 50
+const val WEATHER_GOOD = true
+
+fun main() {
+    val shipNotDamage: Boolean = readln().toBoolean()
     val crewCount: Int = readln().toInt()
     val provisionCount: Int = readln().toInt()
     val isWeatherFavorable: Boolean = readln().toBoolean()
 
-    println(!hasHullDamage && crewCount in 55..70 && provisionCount > 50 ||
-            crewCount == 70 && isWeatherFavorable && provisionCount >= 50)
+    println(
+        (shipNotDamage == SHIP_NOT_DAMAGED) &&
+        (crewCount in MIN_CREW_MEMBERS..MAX_CREW_MEMBERS) &&
+        (provisionCount > MIN_PROVISION_COUNT) ||
+        (crewCount == MAX_CREW_MEMBERS) &&
+        (isWeatherFavorable == WEATHER_GOOD) &&
+        (provisionCount >= MIN_CREW_MEMBERS))
 }
