1bd59f692cf67b7401e7b825f81fb397bc72595d
cade <river1988@mail.ru>
2024-03-28 09:33:53 +0400

    KS-5-1 - добавил функцию генерации числа - убрал магические числа

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,9 +1,17 @@
 package lesson_5
+import kotlin.random.Random
 
 fun main() {
-    print("Решите пример: 5 + 1\nВведите ответ: ")
+    val number1 = getRandomNumber()
+    val number2 = getRandomNumber()
+
+    print("Решите пример: $number1 + $number2\nВведите ответ: ")
     val answer = readln().toInt()
 
-    if (answer == 6) println("Добро пожаловать!")
+    if (answer == (number1 + number2)) println("Добро пожаловать!")
     else println("Доступ запрещен.")
+}
+
+fun getRandomNumber(): Int {
+    return Random.nextInt(10)
 }
