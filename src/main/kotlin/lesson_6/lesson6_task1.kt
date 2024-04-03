d1e41ad4ea5f675fb0abdf184e61907b7ba2fab4
cade <river1988@mail.ru>
2024-04-03 21:38:45 +0400

    KS-6-1 - Логика проверки логина и пароля

---
+++ src/main/kotlin/lesson_6/lesson6_task1.kt
@@ -0,0 +1,23 @@
+package lesson_6
+
+fun main() {
+    print("Введите ваш логин: ")
+    val login = readln()
+    print("Введите ваш пароль: ")
+    val password = readln()
+
+    while (true) {
+        print("Для вхоода в приложение введите ваш логин: ")
+        val enterLogin = readln()
+
+        print("Для вхоода в приложение введите ваш пароль: ")
+        val enterPassword = readln()
+
+        if (login == enterLogin && password == enterPassword) {
+            println("Вы авторизованы!")
+            break
+        } else {
+            println("Неверный пароль, повторите еще раз!")
+        }
+    }
+}
