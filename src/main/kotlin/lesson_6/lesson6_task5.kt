7dfb7af6b411f89ff9a9b62a77d04b77bea48a58
cade <river1988@mail.ru>
2024-04-04 18:01:47 +0400

    KS-6-5 - Пришел к такому варианту рефаторинга

--- src/main/kotlin/lesson_6/lesson6_task5.kt
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -6,50 +6,47 @@ import kotlin.random.Random
 fun main() {
     var attempts = 3
 
-    while (attempts >= 0) {
+    do {
         val num1 = getRandomNumber()
         val num2 = getRandomNumber()
-
-        if (attempts == 0) {
-            println("Доступ запрещен!")
-            break
-        }
 
         print("Решите пример $num1 + $num2 = ")
         val userAnswer = readln().toInt()
 
-        if (num1 + num2 != userAnswer) {
+        if (num1 + num2 == userAnswer) {
+            attempts = 0
+            userLogin()
+        } else {
             println("Ответ не верный, попробуй еще раз")
             attempts--
-        } else {
-            attempts = -1
-            println("Добро пожаловать!")
-            print("Введите ваш логин: ")
-            val login = readln()
-            print("Введите ваш пароль: ")
-            val password = readln()
-
-            while (true) {
-                print("Для вхоода в приложение введите ваш логин: ")
-                val enterLogin = readln()
-
-                print("Для вхоода в приложение введите ваш пароль: ")
-                val enterPassword = readln()
-
-                if (login == enterLogin && password == enterPassword) {
-                    println("Вы авторизованы!")
-                    break
-                } else {
-                    println("Неверный пароль, повторите еще раз!")
-                }
-            }
+            if (attempts == 0) println("Доступ запрещен!")
         }
-
-    }
-
-
+    } while (attempts > 0)
 }
 
 fun getRandomNumber(): Int {
     return Random.nextInt(1, 10)
+}
+
+fun userLogin() {
+    println("Добро пожаловать!")
+    print("Введите ваш логин: ")
+    val login = readln()
+    print("Введите ваш пароль: ")
+    val password = readln()
+
+    do {
+        print("Для вхоода в приложение введите ваш логин: ")
+        val enterLogin = readln()
+
+        print("Для вхоода в приложение введите ваш пароль: ")
+        val enterPassword = readln()
+
+        if (login == enterLogin && password == enterPassword) {
+            println("Вы авторизованы!")
+            break
+        } else {
+            println("Неверный пароль, повторите еще раз!")
+        }
+    } while (login != enterLogin && password != enterPassword)
 }
