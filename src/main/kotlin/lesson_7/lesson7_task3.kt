9f96eb80ca9e2ba1ab29ff690e66ccdb5975948d
cade <river1988@mail.ru>
2024-04-04 20:20:12 +0400

    KS-7-3 - Запись четных чисел. - Верно ли понял задачу?

---
+++ src/main/kotlin/lesson_7/lesson7_task3.kt
@@ -0,0 +1,12 @@
+package lesson_7
+
+fun main(){
+    print("Введите число: ")
+    val userNumber = readln().toInt()
+    val number = StringBuilder()
+
+    for (i in 0 until  userNumber+1  step 2){
+        number.append(i)
+    }
+    println(number)
+}
