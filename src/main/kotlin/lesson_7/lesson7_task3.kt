2154f0de33bb850619da7e67c8d170d1fe40fd73
cade <river1988@mail.ru>
2024-04-05 16:02:29 +0400

    KS-7-3 - форматирвоание кода

--- src/main/kotlin/lesson_7/lesson7_task3.kt
+++ src/main/kotlin/lesson_7/lesson7_task3.kt
@@ -1,11 +1,11 @@
 package lesson_7
 
-fun main(){
+fun main() {
     print("Введите число: ")
     val userNumber = readln().toInt()
     val number = StringBuilder()
 
-    for (i in 0 until  userNumber+1  step 2){
+    for (i in 0 until userNumber + 1 step 2) {
         number.append(i)
     }
     println(number)
