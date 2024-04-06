2fdc0f06532e6e060fdc442d071d4fcb2121ea22
cade <river1988@mail.ru>
2024-04-06 21:09:36 +0400

    KS-6-5 - добавил проверку правильности сгенерированого кода

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -1,28 +1,29 @@
 package lesson_7
 
-import kotlin.random.Random
 
 fun main() {
-    val charsLow = 'A'..'z'
-    val charsUp = 'A'..'Z'
-    val chars = charsUp + charsLow
-    val numbers = 0..9
-    val password = StringBuilder()
-
     print("Минимальная длинна пароля 6 символов.\nВведите длинну пароля: ")
     var passwordLength = readln().toInt()
     if (passwordLength < 6) {
         println("Минимальнная длинна пароля 6 символов. Длинна установлена в 6 символов!")
         passwordLength = 6
     }
-    for (i in 1..passwordLength) {
-        if (getRandomNumber() % 2 == 0) password.append(chars.random())
-        else password.append(numbers.random())
-    }
+    val password = generatePassword(passwordLength)
     println(password)
 }
 
+fun generatePassword(passwordLength: Int): StringBuilder {
+    val charsLow = 'a'..'z'
+    val charsUp = 'A'..'Z'
+    val numbers = 0..9
+    val chars = charsUp + charsLow + numbers
+    val password = StringBuilder()
 
-fun getRandomNumber(): Int {
-    return Random.nextInt(1, 9)
+    do {
+        password.clear()
+        for (i in 1..passwordLength) {
+            password.append(chars.random())
+        }
+    } while (!(password.any { it.isDigit() } && password.any { it.isLowerCase() } && password.any { it.isUpperCase() }))
+    return password
 }
