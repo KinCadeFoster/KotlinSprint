4d87fbdc31b05a9682150432089bc3f46a3872df
cade <river1988@mail.ru>
2024-04-13 22:01:42 +0400

    10-3 - геренарция паароля

---
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -0,0 +1,24 @@
+package lesson_10
+
+fun main() {
+    print("Введите длинну пароля: ")
+    val passwordLength = readln().toInt()
+    val password = generatePassword(passwordLength)
+    println(password)
+}
+
+fun generatePassword(passwordLength: Int): StringBuilder {
+    val specialChars = "!\"#\$%&'()*+,-./"
+    val numbers = 0..9
+    val password = StringBuilder()
+
+
+    for (i in 1..passwordLength) {
+        if (i % 2 == 0) {
+            password.append(specialChars.random())
+        } else {
+            password.append(numbers.random())
+        }
+    }
+    return password
+}
