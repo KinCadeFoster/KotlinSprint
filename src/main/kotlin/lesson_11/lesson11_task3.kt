532bb5ecbe5f1815d53159ce92b64b52a77836d2
cade <river1988@mail.ru>
2024-04-17 23:54:30 +0400

    11-3 - рефакторинг

--- src/main/kotlin/lesson_11/lesson11_task3.kt
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -1,26 +1,68 @@
 package lesson_11
 
+class Room(
+    val cover: String,
+    val roomName: String,
+    val userList: MutableList<MutableMap<String, Any>>,
+    private val myListStatus: List<String> = listOf("микрофон выключен", "разговаривает", "пользователь заглушен"),
+) {
+    fun addUserToRoom(user: User) {
+        val userData = mutableMapOf<String, Any>()
+        userData["nickName"] = (user.nickName)
+        userData["avatar"] = (user.avatar)
+        userData["speakStatus"] = (myListStatus[0])
+
+        userList.add(userData)
+
+    }
+
+    fun updateStatusBadge(nickName: String, newBadgeStatus: Int) {
+        userList.forEach { it ->
+            if (it["nickName"] == nickName) {
+                it["speakStatus"] = myListStatus[newBadgeStatus]
+            }
+        }
+    }
+
+    fun getTextBadgeStatus(nickName: String): () -> String {
+        userList.forEach { it ->
+            if (it["nickName"] == nickName) {
+                return { it["speakStatus"].toString() }
+            }
+        }
+        return { "Статус не найден" }
+    }
+
+    override fun toString(): String {
+        return "User(cover='$cover', roomName='$roomName', userList='$userList')"
+    }
+}
+
+class User(
+    val nickName: String,
+    val avatar: String,
+
+    ) {
+    override fun toString(): String {
+        return "User(nickname='$nickName', login='$avatar')"
+    }
+}
+
+
 fun main() {
     val user1 = User(
-        uid = "1",
-        login = "123",
-        password = "123",
-        email = "123@123.ru",
+        nickName = "Васян",
+        avatar = "avatar.jpeg"
     )
     val room1 = Room(
         cover = "picture.jpg",
         roomName = "Chat",
         userList = mutableListOf()
     )
-
-    println(user1)
-    println(room1)
-
     room1.addUserToRoom(user1)
-    room1.updateStatusBadge(user1, 2)
-    println(room1.getTextBadgeStatus(user1))
+    room1.updateStatusBadge(user1.nickName, 2)
+    println(room1.getTextBadgeStatus(user1.nickName)())
 
     println(user1)
     println(room1)
 }
-
