73626765e851a3022079c2516931af93265d07f9
cade <river1988@mail.ru>
2024-04-20 19:00:20 +0400

    11-3 - рефакторинг

--- src/main/kotlin/lesson_11/lesson11_task1.kt
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -1,4 +1,16 @@
-package lesson_11
+class User(
+    val uid: String,
+    val login: String,
+    val password: String,
+    val email: String,
+    val myListStatus: List<String> = listOf("микрофон выключен", "разговаривает", "пользователь заглушен"),
+    var textBadge: String = myListStatus[0],
+
+    ) {
+    override fun toString(): String {
+        return "User(uid='$uid', login='$login', login='$password', email='$email', textBadge='$textBadge')"
+    }
+}
 
 fun main() {
     val user1 = User(
