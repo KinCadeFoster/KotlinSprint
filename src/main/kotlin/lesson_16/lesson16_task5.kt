0dfa6a362244d7b340f00a072b60a928082f2cef
Cade <46201042+KinCadeFoster@users.noreply.github.com>
2024-05-16 20:09:07 +0400

    16-5 - рефакторинг

--- src/main/kotlin/lesson_16/lesson16_task5.kt
+++ src/main/kotlin/lesson_16/lesson16_task5.kt
@@ -1,5 +1,7 @@
 package lesson_16
 
+import kotlin.math.abs
+
 class Hero(
     private val name: String,
     private var health: Int = 100,
@@ -7,30 +9,39 @@ class Hero(
 ) {
 
     fun takingDamage(damage: Int) {
-        health -= damage
-        if (health < 1) die()
-        println("Ваш герой получил $damage едениц урона")
-        heroStatus()
+        health -= abs(damage)
+        if (health < 1)  {
+            println("Ваш герой получил $damage едениц урона.") // Игрок же должен понять от чего погиб)
+            die()
+        }
+        else {
+            println("Ваш герой получил $damage едениц урона.")
+        }
     }
 
     fun healing(heal: Int) {
-        if (health > 0) health += heal
-        println("Ваш герой восполнил $heal едениц урона")
-        heroStatus()
-
+        if (health == 100) {
+            println("Вы здоровы, не нужно попусту тратить припасы!")
+        }
+        else if (health + heal > 100) {
+            println("Вы получили лечения на ${100 - health} едениц, ${health + heal - 100} едениц потратилось в пустую. " +
+                    "Максимальное здровье не может быть более 100 едениц!")
+            health = 100
+        }
+        else if (health + heal in 1..99){
+            health += heal
+            println("Вы получили лечения на $heal едениц.")
+        }
     }
 
     private fun die() {
         impactForce = 0
         health = 0
+        println("Вы мертвы!")
     }
 
     fun heroStatus() {
-        if (health < 1) println(
-            "Уже не важно, вы погибли и " +
-                    "ваш героой: $name, имеет здровье: $health и силу удара: $impactForce"
-        )
-        else println("Ваш героой: $name, имеет здровье: $health и силу удара: $impactForce")
+        println("Ваш героой: $name, имеет здровье: $health и силу удара: $impactForce.")
     }
 }
 
@@ -40,6 +51,12 @@ fun main() {
 
     userHero.heroStatus()
     userHero.takingDamage(99)
+    userHero.heroStatus()
     userHero.healing(10)
+    userHero.heroStatus()
+    userHero.healing(100)
+    userHero.heroStatus()
+    userHero.healing(100)
     userHero.takingDamage(100)
+    userHero.heroStatus()
 }
