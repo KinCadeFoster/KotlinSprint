a8b8f2c7cef28dfbecf3996f0013169ed43dd77f
Cade <46201042+KinCadeFoster@users.noreply.github.com>
2024-05-16 21:10:39 +0400

    17-3 - Реализация скрытых папок

---
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -0,0 +1,34 @@
+package lesson_17
+
+class Folder(folderName: String, numberFiles: Int, folderSecurity: Boolean) {
+    val name = folderName
+        get() {
+            return if (security) {
+                "скрытая папка"
+            } else {
+                field
+            }
+        }
+    val numFiles = numberFiles
+        get() {
+            return if (security) {
+                0
+            } else {
+                field
+            }
+        }
+    private val security = folderSecurity
+
+    fun folderInfo() {
+        println("Название папки: $name, количество файлов в папке: $numFiles")
+    }
+}
+
+
+fun main() {
+    val secureFolder = Folder("Секретная папка", 10, true)
+    val simpleFolder = Folder("Обычная папка", 10, false)
+
+    secureFolder.folderInfo()
+    simpleFolder.folderInfo()
+}
