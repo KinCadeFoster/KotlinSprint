bd45f54c890b300fc52389e586c808e37159c5e7
Cade <46201042+KinCadeFoster@users.noreply.github.com>
2024-05-17 14:34:52 +0400

    17-4 - рефакторинг

--- src/main/kotlin/lesson_17/lesson17_task4.kt
+++ src/main/kotlin/lesson_17/lesson17_task4.kt
@@ -1,7 +1,6 @@
 package lesson_17
 
-class Package(postNumber: Int, location: String) {
-    private val number = postNumber
+class Package(private val postNumber: Int, location: String) {
     private var location = location
         set(value) {
             field = value
@@ -10,7 +9,7 @@ class Package(postNumber: Int, location: String) {
     private var count = 0
 
     fun printTracking() {
-        println("Посылка с №$number находится в/на $location и уже поменяла $count местоположений")
+        println("Посылка с №$postNumber находится в/на $location и уже поменяла $count местоположений")
     }
 
     fun changeLocation(newLocation: String) {
