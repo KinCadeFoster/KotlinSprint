d131818e27081c31d5041081826c4a5366452897
Cade <46201042+KinCadeFoster@users.noreply.github.com>
2024-05-20 00:04:54 +0400

    18-1 - правки по полиморфизму

--- src/main/kotlin/lesson_18/lesson18_task1.kt
+++ src/main/kotlin/lesson_18/lesson18_task1.kt
@@ -1,25 +1,26 @@
 package lesson_18
 
-interface Order {
-    fun printOrder()
-}
-
-class SingleItemOrder(private val orderNumber: Int, private val item: String) : Order {
-    override fun printOrder() {
+class Order {
+    fun printOrder(
+        orderNumber: Int,
+        item: String,
+    ) {
         println("Заказан товар: $item")
     }
-}
 
-class MultipleItemsOrder(private val orderNumber: Int, private val items: List<String>) : Order {
-    override fun printOrder() {
+    fun printOrder(
+        orderNumber: Int,
+        items: List<String>,
+    ) {
         println("Заказаны следующие товары: ${items.joinToString(", ")}")
     }
 }
 
-fun main() {
-    val order1: Order = SingleItemOrder(1, "Видеокарта")
-    val order2: Order = MultipleItemsOrder(2, listOf("Процессор", "Мышь", "Плюмбус"))
 
-    order1.printOrder()
-    order2.printOrder()
+fun main() {
+    val order1 = Order()
+    val order2 = Order()
+
+    order1.printOrder(1, "Видеокарта")
+    order2.printOrder(2, listOf("Процессор", "Мышь", "Плюмбус"))
 }
