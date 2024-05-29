f3c36ede8f5375bceb4fdd82d3ce178d23da4d5e
cade <river1988@mail.ru>
2024-05-29 21:44:27 +0400

    20-2 - реализация лямбда функции для лечения героя

---
+++ src/main/kotlin/lesson_20/lesson20_task2.kt
@@ -0,0 +1,27 @@
+package lesson_20
+
+class Hero (
+    private val heroName: String,
+    var health: Int,
+    val maxHealth: Int,
+){
+    fun getHeroInfo(){
+        println("Герой: $heroName имеет $health едениц здоровья из максимально возможных $maxHealth")
+    }
+}
+
+val healing: (Int, Hero) -> Unit = { healing: Int, hero: Hero ->
+    hero.health += healing
+    if (hero.health > 100) hero.health = hero.maxHealth
+}
+
+
+fun main() {
+    val hero = Hero("Васян", 50,100)
+
+    hero.getHeroInfo()
+    healing(10, hero)
+    hero.getHeroInfo()
+    healing(100, hero)
+    hero.getHeroInfo()
+}
