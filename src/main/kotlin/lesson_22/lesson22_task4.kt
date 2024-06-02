e60604b4022aca2656bf550f20d885741089057f
Cade <46201042+KinCadeFoster@users.noreply.github.com>
2024-06-02 21:50:11 +0400

    22-4 - Добавил класс MainScreenViewModel - поправил логику - добавил getStatus() для получения состояния

--- src/main/kotlin/lesson_22/lesson22_task4.kt
+++ src/main/kotlin/lesson_22/lesson22_task4.kt
@@ -1,55 +1,31 @@
 package lesson_22
 
-//В разработке Android приложений часто используется паттерн MVVM (Model-View-ViewModel) для
-// разделения бизнес-логики и пользовательского интерфейса. ViewModel отвечает за управление
-// состоянием приложения и хранит данные, которые необходимы для отображения на экране.
-//
-//Предположим, что у нас есть MainScreenViewModel первого экрана приложения, для инициализации
-// которого подгружаются данные с сервера. VM хранит единственное свойство mainScreenState.
-//
-//ViewModel будет содержать внутренний data class MainScreenState для хранения стейта (состояния).
-// Состояние экрана включает два свойства: data (загружаемые данные - String) и isLoading
-// (индикатор процесса загрузки, обычно имеет значение по умолчанию false).
-//
-//Создай data class для хранения состояния. При изменении состояния загрузки создается новый
-// объект стейта, копируя текущее состояние с изменением значения поля isLoading.
-//
-//Сымитируй процесс загрузки данных с сервера в методе вью модели loadData(), в частности
-// состояния:
-//
-//- отсутствие данных;
-//- загрузка данных;
-//- наличие загруженных данных.
+class MainScreenViewModel{
+    data class MainScreenState(
+        var data: String? = null,
+        var isLoading: Boolean = false,
+    )
+    private var state = MainScreenState()
 
-data class MainScreenState(
-    var data: String = "",
-    var isLoading: Boolean = false,
-){
-    fun loadData(data: String, isLoading: Boolean): MainScreenState {
-        if (data.isEmpty()) {
-            println("Данных нет")
-            return this.copy()
-        }
-        else if (isLoading) {
-            println("Выполняетсы загрузка")
-            return this.copy(data = data, isLoading = true)
-        }
-        else {
-            println("Данные получены")
-            return this.copy(data = data, isLoading = false)
-        }
+    fun getStatus() {
+        if (state.isLoading) println("Происходит загрузка данных")
+        else if (state.data == null) println("Данные отсутствуют")
+        else println("Все в полрядке, данные загружены")
+    }
+
+    fun loadData(){
+        state = MainScreenState(isLoading = true)
+
+        getStatus()
+        val data = "Полученные данные"
+        state = MainScreenState(data = data, isLoading = false)
     }
 }
 
+
 fun main() {
-    var state = MainScreenState()
-
-    state = state.loadData("", false)
-    println(state)
-
-    state = state.loadData("Тестовые данные", true)
-    println(state)
-
-    state = state.loadData("Тестовые данные", false)
-    println(state)
+    val state = MainScreenViewModel()
+    state.getStatus()
+    state.loadData()
+    state.getStatus()
 }
